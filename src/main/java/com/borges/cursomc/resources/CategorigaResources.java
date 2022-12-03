23d96ac65f3f8f632fcbdf7b616dc9253685fbf7
Edyborges <ed_borg@hotmail.com>
2022-12-03 10:01:52 -0300

    classe de domínio

--- src/main/java/com/borges/cursomc/resources/CategorigaResources.java
+++ src/main/java/com/borges/cursomc/resources/CategorigaResources.java
@@ -1,15 +1,27 @@
 package com.borges.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.borges.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categoria")
 public class CategorigaResources {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "";
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 =  new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		return lista;
 	} 
 }
