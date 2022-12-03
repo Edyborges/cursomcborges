e6343ae06aacf5d395c58fdf8a76fe9cc8a98fc8
Edyborges <ed_borg@hotmail.com>
2022-12-03 07:27:55 -0300

    começo de tudo

---
+++ src/main/java/com/borges/cursomc/resources/CategorigaResources.java
@@ -0,0 +1,15 @@
+package com.borges.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categoria")
+public class CategorigaResources {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "<h1>HJHJSa</h1>";
+	} 
+}
