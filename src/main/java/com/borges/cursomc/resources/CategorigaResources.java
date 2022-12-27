8f1a14c8f6986062f9d1bc057ef4e9532b5d69c6
Edyborges <ed_borg@hotmail.com>
2022-12-26 21:49:58 -0300

    Paginação Categoria

--- src/main/java/com/borges/cursomc/resources/CategorigaResources.java
+++ src/main/java/com/borges/cursomc/resources/CategorigaResources.java
@@ -1,14 +1,17 @@
 package com.borges.cursomc.resources;
+
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -28,33 +31,43 @@ public class CategorigaResources {
 		Categoria objeto = service.find(id);
 		return ResponseEntity.ok().body(objeto);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria objeto){
+	public ResponseEntity<Void> insert(@RequestBody Categoria objeto) {
 		objeto = service.insert(objeto);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(objeto.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(objeto.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
-	@RequestMapping(value="/{id}",method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria objeto, @PathVariable Integer id){
-		
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria objeto, @PathVariable Integer id) {
+
 		objeto.setId(id);
 		objeto = service.update(objeto);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete (@PathVariable Integer id){
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDto>> findAll() {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDto> listDto = list.stream().map(obj -> new CategoriaDto(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDto>> findPage(
+			@RequestParam(value="page", defaultValue = "0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue = "24")Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue = "nome")String orderBy,
+			@RequestParam(value="direction", defaultValue = "ASC")String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDto> listDto = list.map(obj -> new CategoriaDto(obj));
 		return ResponseEntity.ok().body(listDto);
 	}
 }
--- src/main/java/com/borges/cursomc/services/CategoriaService.java
+++ src/main/java/com/borges/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.borges.cursomc.domain.Categoria;
@@ -19,34 +22,39 @@ public class CategoriaService {
 	private CategoriaRepository repository;
 
 	public Categoria find(Integer id) {
-	   
-	   Optional<Categoria> objeto = repository.findById(id);
-	   
-	   return objeto.orElseThrow(() -> new ObjectNotFoundException(
-			   "Objeto não encontrado ! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-   }
-	
-	public Categoria insert (Categoria objeto) {
+
+		Optional<Categoria> objeto = repository.findById(id);
+
+		return objeto.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado ! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria objeto) {
 		objeto.setId(null);
 		return repository.save(objeto);
 	}
-	
-	public Categoria update (Categoria objeto) {
+
+	public Categoria update(Categoria objeto) {
 		find(objeto.getId());
 		return repository.save(objeto);
 	}
-	
-	public void delete (Integer id) {
+
+	public void delete(Integer id) {
 		find(id);
 		try {
-		repository.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir uma categoria que contém um produto relacionado a ela.");
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException(
+					"Não é possível excluir uma categoria que contém um produto relacionado a ela.");
 		}
 	}
-	
-	public List<Categoria> findAll (){
+
+	public List<Categoria> findAll() {
 		return repository.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 }
